6716a88d9d99ba8474df3b0d7a2c7f52cb70a9f9
Silvio Giebl <gieblsilvio@outlook.com>
2026-03-07 21:34:12 +0100

    Add MinRequiredGradleVersionTest

---
+++ src/test/kotlin/io/github/sgtsilvio/gradle/proguard/MinRequiredGradleVersionTest.kt
@@ -0,0 +1,75 @@
+package io.github.sgtsilvio.gradle.proguard
+
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+
+/**
+ * @author Silvio Giebl
+ */
+internal class MinRequiredGradleVersionTest {
+
+    @Test
+    fun test(@TempDir projectDir: File) {
+        projectDir.resolve("settings.gradle.kts").writeText(
+            """
+            rootProject.name = "test"
+            """.trimIndent()
+        )
+        projectDir.resolve("build.gradle.kts").writeText(
+            """
+            plugins {
+                java
+                id("io.github.sgtsilvio.gradle.proguard")
+            }
+            repositories {
+                mavenCentral()
+            }
+            java {
+                toolchain {
+                    languageVersion.set(JavaLanguageVersion.of(11))
+                }
+            }
+            val proguardJar by tasks.registering(proguard.taskClass) {
+                addInput { classpath.from(tasks.jar) }
+                addOutput { archiveFile.set(base.libsDirectory.file("test-proguarded.jar")) }
+                jdkModules.add("java.base")
+                mappingFile.set(layout.buildDirectory.file("test-mapping.txt"))
+                rules.add("-keep class test.Main { public static void main(java.lang.String[]); }")
+            }
+            // copy inJars, libraryJars, outJars to check if they are compatible with the configuration cache
+            val copyProguardJars by tasks.registering(Copy::class) {
+                from(proguardJar.map { it.inputClasspath }) { into("inJars") }
+                from(proguardJar.map { it.outputClasspath }) { into("outJars") }
+                from(proguardJar.map { it.libraryClasspath }) { into("libraryJars") }
+                into(layout.buildDirectory.dir("copyProguardJars"))
+            }
+            """.trimIndent()
+        )
+        projectDir.resolve("src/main/java/test/Main.java").apply { parentFile.mkdirs() }.writeText(
+            """
+            package test;
+            public class Main {
+                public static void main(String[] args) {
+                    System.out.println("Hello World!");
+                }
+            }
+            """.trimIndent()
+        )
+
+        val result = GradleRunner.create()
+            .withGradleVersion("6.7")
+            .withProjectDir(projectDir)
+            .withPluginClasspath()
+            .withArguments("copyProguardJars")
+            .build()
+
+        assertFalse(result.output.contains("ProGuard, version")) // stdout should be silenced
+        assertEquals(TaskOutcome.SUCCESS, result.task(":jar")?.outcome)
+        assertEquals(TaskOutcome.SUCCESS, result.task(":proguardJar")?.outcome)
+        assertEquals(TaskOutcome.SUCCESS, result.task(":copyProguardJars")?.outcome)
+    }
+}
